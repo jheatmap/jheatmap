a5704162e411ba30a5a362ab58bc637fddc0595d
Jordi Deu-Pons <jordi@jordeu.net>
2014-01-08 16:35:45 +0100

    New feature, copy to clipboard

--- src/js/jheatmap/Heatmap.js
+++ src/js/jheatmap/Heatmap.js
@@ -31,9 +31,11 @@ jheatmap.Heatmap = function (options) {
         HideSelected: new jheatmap.actions.HideSelected(this),
         ShowHidden: new jheatmap.actions.ShowHidden(this),
         ClearSelection: new jheatmap.actions.ClearSelection(this),
-        Separator: new jheatmap.actions.Separator(),
+        Separator1: new jheatmap.actions.Separator(),
         AggregationSort: new jheatmap.actions.AggregationSort(this),
-        MutualExclusiveSort: new jheatmap.actions.MutualExclusiveSort(this)
+        MutualExclusiveSort: new jheatmap.actions.MutualExclusiveSort(this),
+        Separator2: new jheatmap.actions.Separator(),
+        CopyToClipboard: new jheatmap.actions.CopyToClipboard(this)
     };
 
     /**
---
+++ src/js/jheatmap/actions/CopyToClipboard.js
@@ -0,0 +1,45 @@
+/**
+ * Copy to clipboard selected labels.
+ *
+ * @example
+ * new jheatmap.actions.CopyToClipboard();
+ *
+ * @param heatmap   The target heatmap
+ * @class
+ */
+jheatmap.actions.CopyToClipboard = function (heatmap) {
+    this.heatmap = heatmap;
+    this.title = "Copy to clipboard";
+    this.icon = "fa-clipboard";
+};
+
+/**
+ * Execute the action. *
+ * @private
+ */
+jheatmap.actions.CopyToClipboard.prototype.run = function (dimension) {
+    if (dimension.selected.length > 0) {
+
+        text = "";
+        i = 0;
+        while (i < dimension.selected.length) {
+            var value = dimension.getValue(dimension.selected[i], dimension.selectedValue);
+            text = text + value;
+            i++;
+            if (i < dimension.selected.length) {
+               text = text + ", ";
+            }
+        }
+
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+    }
+};
+
+jheatmap.actions.CopyToClipboard.prototype.rows = function() {
+    this.run(this.heatmap.rows);
+};
+
+jheatmap.actions.CopyToClipboard.prototype.columns = function() {
+    this.run(this.heatmap.cols);
+};
+
